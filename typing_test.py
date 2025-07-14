952ef65120b6d72439a7828da467bcc50f5c1669
Akshita12-tech <akshitajamindar@gmail.com>
2025-07-14 10:38:58 +0100

    Create typing_test.py

---
+++ typing_test.py
@@ -0,0 +1,16 @@
+import time
+
+def typing_test():
+  prompt_text= "The little bunny rabbit was faster than turtle but still the turtle won the race through determination"
+  print("Typing Speed Test")
+
+
+
+
+typing_test()
+
+
+
+
+
+
